dc8d284911d5fcc227d57c1e536b6cbff40c1bc8
Dinesh Babu <dineshbabu853@gmail.com>
2020-12-03 22:24:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+//importing components
+import Titles from "./components/title";
+import Form from "./components/form";
+import Weather from "./components/weather";
+const Api_Key = "8d2de98e089f1c28e1a22fc19a24ef04";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined,
+  };
+
+  getWeather = async (e) => {
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    e.preventDefault();
+    await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
+    )
+      .then(async (res) => {
+        const response = await res.json();
+
+        if (city && country) {
+          this.setState({
+            temperature: response.main.temp,
+            city: response.name,
+            country: response.sys.country,
+            humidity: response.main.humidity,
+            description: response.weather[0].description,
+            error: "",
+          });
+        } else {
+          this.setState({
+            error: "Please input search values...",
+          });
+        }
+      })
+      .catch((err) => {
+        this.setState({
+          error: "Please enter a valid city and country",
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form loadWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/title.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const title = () => {
+  return (
+    <div>
+      <h1 className="title-container__title">Weather App</h1>
+      <p className="title-container__subtitle"> Know your weather </p>
+    </div>
+  );
+};
+
+export default title;
